9c448b9427a4a79aea3e1c67aa7ba4fc51ca36fc
pipixia <1546712965@qq.com>
2023-06-16 20:50:00 +0800

    first commit

---
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvStartup.java
@@ -0,0 +1,251 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.namesrv;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.rocketmq.common.ControllerConfig;
+import org.apache.rocketmq.common.MQVersion;
+import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.common.constant.LoggerName;
+import org.apache.rocketmq.common.namesrv.NamesrvConfig;
+import org.apache.rocketmq.controller.ControllerManager;
+import org.apache.rocketmq.logging.org.slf4j.Logger;
+import org.apache.rocketmq.remoting.netty.NettyClientConfig;
+import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
+import org.apache.rocketmq.srvutil.ServerUtil;
+import org.apache.rocketmq.srvutil.ShutdownHookThread;
+
+public class NamesrvStartup {
+
+    private final static Logger log = LoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
+    private final static Logger logConsole = LoggerFactory.getLogger(LoggerName.NAMESRV_CONSOLE_LOGGER_NAME);
+    private static Properties properties = null;
+    private static NamesrvConfig namesrvConfig = null;
+    private static NettyServerConfig nettyServerConfig = null;
+    private static NettyClientConfig nettyClientConfig = null;
+    private static ControllerConfig controllerConfig = null;
+
+    public static void main(String[] args) {
+        main0(args);
+        controllerManagerMain();
+    }
+
+    public static NamesrvController main0(String[] args) {
+        try {
+            parseCommandlineAndConfigFile(args);
+            NamesrvController controller = createAndStartNamesrvController();
+            return controller;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        return null;
+    }
+
+    public static ControllerManager controllerManagerMain() {
+        try {
+            if (namesrvConfig.isEnableControllerInNamesrv()) {
+                return createAndStartControllerManager();
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return null;
+    }
+
+    // 解析命令以及配置文件
+    public static void parseCommandlineAndConfigFile(String[] args) throws Exception {
+        // 设置版本号为当前版本号
+        System.setProperty(RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
+
+        //构造org.apache.commons.cli.Options,并添加-h -n参数，-h参数是打印帮助信息，-n参数是指定namesrvAddr
+        Options options = ServerUtil.buildCommandlineOptions(new Options());
+        //初始化commandLine，并在options中添加-c -p参数，-c指定nameserver的配置文件路径，-p标识打印配置信息
+        CommandLine commandLine = ServerUtil.parseCmdLine("mqnamesrv", args, buildCommandlineOptions(options), new DefaultParser());
+        if (null == commandLine) {
+            System.exit(-1);
+            return;
+        }
+
+        // nameserver配置类，业务参数
+        namesrvConfig = new NamesrvConfig();
+        //netty服务器配置类，网络参数
+        nettyServerConfig = new NettyServerConfig();
+        nettyClientConfig = new NettyClientConfig();
+        //设置nameserver的端口号
+        nettyServerConfig.setListenPort(9876);
+        //命令带有-c参数，说明指定配置文件，需要根据配置文件路径读取配置文件内容，并将文件中配置信息赋值给NamesrvConfig和NettyServerConfig
+        if (commandLine.hasOption('c')) {
+            String file = commandLine.getOptionValue('c');
+            if (file != null) {
+                InputStream in = new BufferedInputStream(Files.newInputStream(Paths.get(file)));
+                properties = new Properties();
+                properties.load(in);
+                MixAll.properties2Object(properties, namesrvConfig);
+                MixAll.properties2Object(properties, nettyServerConfig);
+                MixAll.properties2Object(properties, nettyClientConfig);
+                if (namesrvConfig.isEnableControllerInNamesrv()) {
+                    controllerConfig = new ControllerConfig();
+                    MixAll.properties2Object(properties, controllerConfig);
+                }
+                namesrvConfig.setConfigStorePath(file);
+
+                System.out.printf("load config properties file OK, %s%n", file);
+                in.close();
+            }
+        }
+
+         //解析命令行参数，并加载到namesrvConfig中
+        MixAll.properties2Object(ServerUtil.commandLine2Properties(commandLine), namesrvConfig);
+        //命令行带有-p，说明是打印参数的命令，那么就打印出NamesrvConfig和NettyServerConfig的属性。在启动NameServer时可以先使用./mqnameserver -c configFile -p打印当前加载的配置属性
+        if (commandLine.hasOption('p')) {
+            MixAll.printObjectProperties(logConsole, namesrvConfig);
+            MixAll.printObjectProperties(logConsole, nettyServerConfig);
+            MixAll.printObjectProperties(logConsole, nettyClientConfig);
+            if (namesrvConfig.isEnableControllerInNamesrv()) {
+                MixAll.printObjectProperties(logConsole, controllerConfig);
+            }
+            System.exit(0);
+        }
+
+        //检查ROCKETMQ_HOME，不能为空
+//        namesrvConfig.setRocketmqHome("pipiban_待删除");
+        if (null == namesrvConfig.getRocketmqHome()) {
+            System.out.printf("Please set the %s variable in your environment to match the location of the RocketMQ installation%n", MixAll.ROCKETMQ_HOME_ENV);
+            System.exit(-2);
+        }
+        MixAll.printObjectProperties(log, namesrvConfig);
+        MixAll.printObjectProperties(log, nettyServerConfig);
+
+    }
+
+    public static NamesrvController createAndStartNamesrvController() throws Exception {
+
+        NamesrvController controller = createNamesrvController();
+        start(controller);
+        NettyServerConfig serverConfig = controller.getNettyServerConfig();
+        String tip = String.format("The Name Server boot success. serializeType=%s, address %s:%d", RemotingCommand.getSerializeTypeConfigInThisServer(), serverConfig.getBindAddress(), serverConfig.getListenPort());
+        log.info(tip);
+        System.out.printf("%s%n", tip);
+        return controller;
+    }
+
+    public static NamesrvController createNamesrvController() {
+
+        final NamesrvController controller = new NamesrvController(namesrvConfig, nettyServerConfig, nettyClientConfig);
+        // remember all configs to prevent discard
+        controller.getConfiguration().registerConfig(properties);
+        return controller;
+    }
+
+    public static NamesrvController start(final NamesrvController controller) throws Exception {
+
+        if (null == controller) {
+            throw new IllegalArgumentException("NamesrvController is null");
+        }
+
+        boolean initResult = controller.initialize();
+        if (!initResult) {
+            controller.shutdown();
+            System.exit(-3);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownHookThread(log, (Callable<Void>) () -> {
+            controller.shutdown();
+            return null;
+        }));
+
+        controller.start();
+
+        return controller;
+    }
+
+    public static ControllerManager createAndStartControllerManager() throws Exception {
+        ControllerManager controllerManager = createControllerManager();
+        start(controllerManager);
+        String tip = "The ControllerManager boot success. serializeType=" + RemotingCommand.getSerializeTypeConfigInThisServer();
+        log.info(tip);
+        System.out.printf("%s%n", tip);
+        return controllerManager;
+    }
+
+    public static ControllerManager createControllerManager() throws Exception {
+        NettyServerConfig controllerNettyServerConfig = (NettyServerConfig) nettyServerConfig.clone();
+        ControllerManager controllerManager = new ControllerManager(controllerConfig, controllerNettyServerConfig, nettyClientConfig);
+        // remember all configs to prevent discard
+        controllerManager.getConfiguration().registerConfig(properties);
+        return controllerManager;
+    }
+
+    public static ControllerManager start(final ControllerManager controllerManager) throws Exception {
+
+        if (null == controllerManager) {
+            throw new IllegalArgumentException("ControllerManager is null");
+        }
+
+        boolean initResult = controllerManager.initialize();
+        if (!initResult) {
+            controllerManager.shutdown();
+            System.exit(-3);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownHookThread(log, (Callable<Void>) () -> {
+            controllerManager.shutdown();
+            return null;
+        }));
+
+        controllerManager.start();
+
+        return controllerManager;
+    }
+
+    public static void shutdown(final NamesrvController controller) {
+        controller.shutdown();
+    }
+
+    public static void shutdown(final ControllerManager controllerManager) {
+        controllerManager.shutdown();
+    }
+
+    public static Options buildCommandlineOptions(final Options options) {
+        Option opt = new Option("c", "configFile", true, "Name server config properties file");
+        opt.setRequired(false);
+        options.addOption(opt);
+
+        opt = new Option("p", "printConfigItem", false, "Print all config items");
+        opt.setRequired(false);
+        options.addOption(opt);
+        return options;
+    }
+
+    public static Properties getProperties() {
+        return properties;
+    }
+}
